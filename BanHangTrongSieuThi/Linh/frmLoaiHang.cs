895fc2fbaffa045e992ba7135011610024b9f8cb
kemshiro <i.am.nguyentuanlinh@gmail.com>
2017-05-25 19:26:55 +0700

    Thêm 2 form sửa csdl

---
+++ BanHangTrongSieuThi/Linh/frmLoaiHang.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BanHangTrongSieuThi.Linh
+{
+    public partial class frmLoaiHang : Form
+    {
+        public frmLoaiHang()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLoaiHang_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- DAO/DataBase/dbo.NhanVien.sql
+++ DAO/DataBase/dbo.NhanVien.sql
@@ -7,19 +7,36 @@ Create TABLE [dbo].[NhanVien] (
     PRIMARY KEY CLUSTERED ([MaNV] ASC)
 );
 go
+
 --duc anh tao proc lay ra manv gan nhat
-alter proc ManvNext(@manv int output)
+create proc ManvNext(@manv int output)
 as
 begin
 	select @manv =  IDENT_CURRENT('NhanVien')
+end
+
+--Linh tao bang Loai Hang
+CREATE TABLE [dbo].[LoaiHang] (
+    [MaLoaiHang]  INT           IDENTITY (1, 1) NOT NULL,
+    [TenLoaiHang] NVARCHAR (50) NULL,
+    CONSTRAINT [PK_LoaiHang] PRIMARY KEY CLUSTERED ([MaLoaiHang] ASC),
+);
+go
+
+--Linh tao proc LoaiHang gan nhat
+create proc LoaiHangGanNhat(@MaLoaiHang int output)
+as
+begin
+	select @MaLoaiHang =  IDENT_CURRENT('LoaiHang')
 end
 
 --khoa tao bang gian hang
 CREATE TABLE [dbo].[GianHang] (
     [MaGianHang]  INT           IDENTITY (1, 1) NOT NULL,
     [TenGianHang] NVARCHAR (50) NULL,
-    [LoaiHangMa]  INT           NULL,
-    CONSTRAINT [PK_GianHang] PRIMARY KEY CLUSTERED ([MaGianHang] ASC)
+    [LoaiHangMa]  INT           NOT NULL,
+    CONSTRAINT [PK_GianHang] PRIMARY KEY CLUSTERED ([MaGianHang] ASC),
+	CONSTRAINT [FK_GianHang_LoaiHang]  FOREIGN KEY ([LoaiHangMa]) REFERENCES [dbo].[LoaiHang] ([MaLoaiHang])
 );
 go
 
